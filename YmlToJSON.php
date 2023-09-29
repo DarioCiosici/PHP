98c3728a237535d928c89702a03ad8d94b2be538
DarioCiosici <94352108+DarioCiosici@users.noreply.github.com>
2023-09-29 08:23:46 +0200

    Yml To JSON

---
+++ YmlToJSON.php
@@ -0,0 +1,9 @@
+<?php
+use Symfony\Component\Yaml\Yaml;
+$filepath='dati.ylm';
+$output_file='dati.json';
+$file=file_get_contents($filepath);
+$yalmdata=Yalm::parse($file);
+$jsonData = json_encode($phpData, JSON_PRETTY_PRINT);
+file_put_contents($output_file, $jsonData);
+?>
