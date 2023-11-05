d96caea80a5b280da7b3b7883f8505b09695e281
DarioCiosici <94352108+DarioCiosici@users.noreply.github.com>
2023-11-05 23:50:07 +0100

    Concerto pezzi

--- YmlToJSON.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Symfony\Component\Yaml\Yaml;
-$filepath='dati.ylm';
-$output_file='dati.json';
-$file=file_get_contents($filepath);
-$yalmdata=Yalm::parse($file);
-$jsonData = json_encode($yalmdata, JSON_PRETTY_PRINT);
-file_put_contents($output_file, $jsonData);
-?>
